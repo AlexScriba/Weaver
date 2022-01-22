811cd5df6ca8ad71e4e92bbde953ae4303ae16e5
AlexScriba <61542302+AlexScriba@users.noreply.github.com>
2022-01-22 13:58:40 +0800

    Added Padding, Margin, Center and some basic styling

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,12 @@ namespace Weaver {
 
 	interface WeaverElement {
 		tag: string;
-		type: 'content' | 'single-child' | 'multi-child';
+		type: 'content' | 'single-child' | 'multi-child' | 'styling';
 		class?: string;
 	}
 
 	interface ContentElement extends WeaverElement {
-		content: string;
+		content: string | number;
 		type: 'content';
 	}
 
@@ -29,6 +29,33 @@ namespace Weaver {
 	interface MultiChildElement extends WeaverElement {
 		elements: ChildElement[];
 		type: 'multi-child';
+	}
+
+	interface StylingElement extends Omit<SingleChildElement, 'type'> {
+		type: 'styling';
+		style: Style;
+	}
+
+	// -------------------------------- COMPONENTS --------------------------------
+
+	interface Style {
+		height?: number;
+		width?: number;
+
+		paddingTop?: number;
+		paddingBottom?: number;
+		paddingLeft?: number;
+		paddingRight?: number;
+
+		marginTop?: number;
+		marginBottom?: number;
+		marginLeft?: number;
+		marginRight?: number;
+
+		display?: string;
+		flexDirection?: string;
+		justifyContent?: string;
+		alignItems?: string;
 	}
 
 	// -------------------------------- COMPONENTS --------------------------------
@@ -53,7 +80,7 @@ namespace Weaver {
 
 	const ContentComponent = (
 		tag: string,
-		content: string,
+		content: string | number,
 		props?: ContentProps
 	): ContentElement => {
 		return {
@@ -76,15 +103,114 @@ namespace Weaver {
 		};
 	};
 
+	interface StylingProps {
+		style: Style;
+		child: WeaverElement;
+	}
+
+	const StylingComponent = ({ style, child }: StylingProps): StylingElement => {
+		return {
+			tag: 'div',
+			type: 'styling',
+			style: style,
+			child: child,
+		};
+	};
+
+	interface AppProps {
+		body: WeaverElement;
+	}
+
+	const App = (props: AppProps): SingleChildElement => {
+		return SingleChildComponent('div', { class: 'App', child: props.body });
+	};
+
 	const Div = (props: SingleChildProps): SingleChildElement => {
 		return SingleChildComponent('div', props);
 	};
 
-	const Text = (text: string, props?: ContentProps): ContentElement => {
+	const Text = (text: string | number, props?: ContentProps): ContentElement => {
 		return ContentComponent('div', text, props);
 	};
 
+	const Row = (props: MultiChildProps): MultiChildElement => {
+		let appendedClass = 'row';
+
+		if (props.class) appendedClass += ' ' + props.class;
+
+		return MultiChildComponent('div', { ...props, class: appendedClass });
+	};
+
+	const Column = (props: MultiChildProps): MultiChildElement => {
+		let appendedClass = 'column';
+
+		if (props.class) appendedClass += ' ' + props.class;
+
+		return MultiChildComponent('div', { ...props, class: appendedClass });
+	};
+
+	const Padding = (padding: number, child: WeaverElement): StylingElement => {
+		return StylingComponent({
+			child: child,
+			style: {
+				paddingTop: padding,
+				paddingBottom: padding,
+				paddingLeft: padding,
+				paddingRight: padding,
+			},
+		});
+	};
+
+	const Margin = (margin: number, child: WeaverElement): StylingElement => {
+		return StylingComponent({
+			child: child,
+			style: {
+				marginTop: margin,
+				marginBottom: margin,
+				marginLeft: margin,
+				marginRight: margin,
+			},
+		});
+	};
+
+	const Center = (child: WeaverElement): StylingElement => {
+		return StylingComponent({
+			child: child,
+			style: {
+				height: 100,
+				width: 100,
+				justifyContent: 'center',
+				alignItems: 'center',
+			},
+		});
+	};
+
 	// -------------------------------- WEAVER DOM --------------------------------
+
+	const NumToPix = (num: number | undefined) => {
+		const pix = num || 0;
+		return '' + num + 'px';
+	};
+
+	const configureStyle = (elementStyle: CSSStyleDeclaration, style: Style) => {
+		elementStyle.height = style.height + '%';
+		elementStyle.width = style.width + '%';
+
+		elementStyle.paddingTop = NumToPix(style.paddingTop);
+		elementStyle.paddingBottom = NumToPix(style.paddingBottom);
+		elementStyle.paddingLeft = NumToPix(style.paddingLeft);
+		elementStyle.paddingRight = NumToPix(style.paddingRight);
+
+		elementStyle.marginTop = NumToPix(style.marginTop);
+		elementStyle.marginBottom = NumToPix(style.marginLeft);
+		elementStyle.marginLeft = NumToPix(style.marginLeft);
+		elementStyle.marginRight = NumToPix(style.paddingRight);
+
+		elementStyle.display = style.display || 'block';
+		elementStyle.flexDirection = style.flexDirection || '';
+		elementStyle.justifyContent = style.justifyContent || '';
+		elementStyle.alignItems = style.alignItems || '';
+	};
 
 	const createElements = (rootElement: WeaverElement): HTMLElement => {
 		const element = document.createElement(rootElement.tag);
@@ -92,7 +218,7 @@ namespace Weaver {
 		switch (rootElement.type) {
 			case 'content':
 				const contentElement = rootElement as ContentElement;
-				element.innerHTML = contentElement.content;
+				element.innerHTML = '' + contentElement.content;
 				break;
 			case 'single-child':
 				const singleChildElement = rootElement as SingleChildElement;
@@ -105,6 +231,11 @@ namespace Weaver {
 					element.appendChild(createElements(child));
 				}
 				break;
+			case 'styling':
+				const styleElement = rootElement as StylingElement;
+				configureStyle(element.style, styleElement.style);
+				element.appendChild(createElements(styleElement.child));
+				break;
 		}
 
 		if (rootElement.class) element.className = rootElement.class;
@@ -114,12 +245,27 @@ namespace Weaver {
 
 	// -------------------------------- LOGIC --------------------------------
 
-	const builtApp = Div({
-		child: MultiChildComponent('div', { elements: [Text('Item 1'), Text('Item 2')] }),
-		class: 'mainDiv card',
+	const builtApp = App({
+		body: Column({
+			elements: [
+				Text('Items 1'),
+				Row({
+					elements: [
+						Text('Hello world'),
+						Padding(
+							20,
+							Column({
+								elements: [Margin(10, Text('Hello')), Margin(10, Text(20))],
+							})
+						),
+					],
+				}),
+			],
+		}),
 	});
 
 	const element = createElements(builtApp);
+	console.log(builtApp);
 
 	document.body.appendChild(element);
 }
